a4a11961cef0a775954ccd02a758dd166f57531f
Kat Hoffman <hoffmakl@umich.edu>
2023-08-25 10:08:50 -0400

    move max_time to cp_dt data

--- scripts/2-create_date_times.R
+++ scripts/2-create_date_times.R
@@ -20,13 +20,13 @@ cp_dt <-
          Y_time = death_dt,
          Cens_time
   ) |>
-  mutate(t1_end = t1_start + hours(24),
+  mutate(max_time = pmax(Y_time, Cens_time, na.rm = T),
          M_time = case_when(M_time > Y_time ~ Y_time - hours(1), # fix temporality of a few AKIs that are after death
                             M_time > Cens_time ~ NA_POSIXct_, # some patients had AKI detected thru labs collected after discharge, mark these AKI instances as NA because we don't care about it (and will mess up our coding logic later)
                             TRUE ~ M_time)) |>
   filter(A_time >= t1_start | is.na(A_time), # make sure all times are sensical (n=22, 0.6% observations removed for data entry error)
          M_time >= t1_start | is.na(M_time),
          Y_time >= t1_start | is.na(Y_time),
-         Cens_time >= t1_start | is.na(Cens_time))
+         Cens_time >= t1_start | is.na(Cens_time)) 
 
 saveRDS(cp_dt, here::here("data/derived/dts_cohort.rds"))
