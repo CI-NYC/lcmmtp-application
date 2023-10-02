442a9c9647d9ec3a3141c6bcfb21e636cee77cd7
Kat Hoffman <hoffmakl@umich.edu>
2023-10-01 21:57:57 -0700

    extend to all 14 time points

--- scripts/6-run_lcmmtp.R
+++ scripts/6-run_lcmmtp.R
@@ -16,6 +16,8 @@ test <-
   all_wide |>
     select(empi,
            group,
+           # t1_start,
+           # A_time, M_time, Y_time, Cens_time,
            L_value_1_glucose,
            L_value_2_glucose,
            A_1,
@@ -26,38 +28,49 @@ test <-
            M_2,
            Y_1,
            Y_2,
-           C_1,
-           C_2
-           )
-  
+           Observed_1,
+           Observed_2
+           ) 
 
-# An `lcmmtp_variables` object mapping observed variables to the assumed variable structure.
+max_window <- 14
+As <- paste0("A_", 1:max_window)
+Ms <- paste0("M_", 1:max_window)
+Ys <- paste0("Y_", 1:max_window)
+Cs <- paste0("Observed_", 1:max_window)
+Ls <- as.list(paste0("L_value_", 1:max_window, "_glucose"))
+Zs <- as.list(paste0("Z_value_", 1:max_window, "_glucose"))
+
 vars <- lcmmtp:::lcmmtp_variables$new(
-  L = list(c("L_value_1_glucose"), c("L_value_2_glucose")),
-  A = c("A_1", "A_2"),
-  Z = list(c("Z_value_1_glucose"), c("Z_value_2_glucose")),
-  M = c("M_1", "M_2"),
-  Y = c("Y_1", "Y_2"),
-  # Y = "Y_2",
-  cens = c("C_1", "C_2")
+  L = Ls,
+  A = As,
+  Z = Zs,
+  M = Ms,
+  Y = Ys,
+  cens = Cs
 )
+# 
+# # An `lcmmtp_variables` object mapping observed variables to the assumed variable structure.
+# vars <- lcmmtp:::lcmmtp_variables$new(
+#   L = list(c("L_value_1_glucose"), c("L_value_2_glucose")),
+#   A = c("A_1", "A_2"),
+#   Z = list(c("Z_value_1_glucose"), c("Z_value_2_glucose")),
+#   M = c("M_1", "M_2"),
+#   Y = c("Y_1", "Y_2"),
+#   # Y = "Y_2",
+#   cens = c("Observed_1", "Observed_2")
+# )
 
-
-view(test)
-
-tmp |>
-  filter(C_2 == 0)
-
-
-fit <- lcmmtp(test,
+lrnrs <- c("glm", "glmnet")
+lrnrs <- c("glm")
+fit <- lcmmtp(all_wide,
               vars,
               d_ap,
-              d_as# ,
-              # "glm",
-              # folds=2
+              d_as,
+              .lcmmtp_control(learners_trt = lrnrs,
+                              learners_mediator = lrnrs,
+                              learners_QL = lrnrs,
+                              learners_QZ = lrnrs,
+                              learners_QM = lrnrs)
 )
-
-test |>
-  filter(C_1==1, is.na(M_2))
 
 fit
